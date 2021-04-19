ca42594d21a7b84283ffcfdd93882b729773ab6a
pckopat21 <servet-avci@hotmail.com>
2021-04-20 01:36:49 +0300

    Memur vefat raporu yazdırma işlemi tamamlandı.

--- app/Models/Admin/Izin_model.php
+++ app/Models/Admin/Izin_model.php
@@ -72,7 +72,7 @@ class Izin_model
     {
         $builder = $this->db->table('izin_calisan_haklari ich');
         $builder->select('i.izin_saglikkurumu,d.durum_ad,u.unvan_ad,p.personel_adsoyad,p.personel_tc,
-        p.personel_sicilno,it.izin_ad,i.izin_tarih,p.personel_adres,
+        p.personel_sicilno,it.izin_ad,i.izin_tarih,p.personel_adres,i.izin_vefat,
         i.izin_adresi,i.izin_yil,i.izin_baslayis,i.izin_suresi,p.personel_telefon,p.personel_isegiristarih');
         $builder->join('durum d',' d.durum_id=ich.calisan_statu_id','INNER');
         $builder->join('personel p','p.unvan_id=d.durum_id','INNER');
